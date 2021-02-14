aa6a750e69f6ced78b58aee79f63292537d41eb9
takanassyi <takanassyi@gmail.com>
2021-02-14 21:27:44 +0900

    add react-mui-tmpl

---
+++ react-mui-tmpl/src/components/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GenericTemplate from '../templates/GenericTemplate';
+
+const HomePage: React.FC = () => (
+  <GenericTemplate title="トップページ">
+    <>トップページ内容</>
+  </GenericTemplate>
+);
+
+export default HomePage;
